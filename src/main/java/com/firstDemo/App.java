41f9e0e8c57a705ec276d1435e955b44752c6947
monalika123456 <sahoomonalika123@gmail.com>
2019-07-31 01:03:43 +0530

    first commit

---
+++ src/main/java/com/firstDemo/App.java
@@ -0,0 +1,16 @@
+package com.firstDemo;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @Test
+    public void firsttest()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
